3f56511be5c38a0c4f325482d57c3671fd5f64ef
candy <448932728@qq.com>
2025-06-17 22:21:44 +0800

    refactor(internal/handler): 重构 WebsocketInit 函数
    -移除了未使用的 context 参数
    - 移除了未使用的 svc.Context 参数
    - 简化了 WebsocketInit 函数的签名

--- internal/handler/ws_routers.go
+++ internal/handler/ws_routers.go
@@ -1,14 +1,11 @@
 package handler
 
 import (
-	"context"
-
 	"go-zero-websocket-demo/infrastructure/pkg/websocketx"
 	"go-zero-websocket-demo/internal/logic"
-	"go-zero-websocket-demo/internal/svc"
 )
 
-func WebsocketInit(ctx context.Context, svcCtx *svc.ServiceContext) {
+func WebsocketInit() {
 	// 创建账户相关路由组
 	accountGroup := websocketx.NewGroup("test")
 
